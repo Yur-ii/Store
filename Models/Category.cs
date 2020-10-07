c46fd8b0d91bfe1b606e7ec2472fd40a66390758
Yur-ii <pysankoyurii@gmail.com>
2020-10-07 17:28:05 +0300

    Corrected the mistake

--- Models/Category.cs
+++ Models/Category.cs
@@ -10,6 +10,6 @@ namespace Store.Models
         public int Id { get; set; }
         public string CategoryName { get; set; }
         public string Desc { get; set; }
-        public List<Product> Products;
+        public List<Product> Products { get; set; }
     }
 }
