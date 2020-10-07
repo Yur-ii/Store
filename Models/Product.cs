e8efbff40b99b1acf15f0d2907de2293ff608329
Yur-ii <pysankoyurii@gmail.com>
2020-10-07 17:26:24 +0300

    Created models

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LongDesc { get; set; }
+        public string ShortDesc { get; set; }
+        public string Img { get; set; }
+        public double Prive { get; set; }
+        public bool IsFavorite { get; set; }
+        public int Avilable { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -29,9 +29,9 @@ namespace Store
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseHttpsRedirection();
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
             app.UseStaticFiles();
-
             app.UseRouting();
 
             app.UseAuthorization();
