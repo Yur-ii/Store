a8ebe22d563c9c39129be1d54b933748de7fdb71
Yur-ii <pysankoyurii@gmail.com>
2020-10-27 18:15:53 +0200

    Add catalog

---
+++ Components/Catalog.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Store.Models;
+
+namespace Store.Components
+{
+    public class Catalog : ViewComponent
+    {
+        private readonly StoreContext db;
+        public Catalog(StoreContext context)
+        {
+            db = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            List<Category> categories = db.Categories.ToList();
+            List<SubCategory> subCategories = db.SubCategories.ToList();
+            return View("Catalog", (categories, subCategories));
+        }
+    }
+}
