0d87787fd3c20146e6d3b20be87efd1a7051e7ac
Yur-ii <pysankoyurii@gmail.com>
2020-10-28 13:46:18 +0200

    Add model for shopping basket

---
+++ Models/ShoppingBasket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    public class ShoppingBasket
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string UserId { get; set; }
+        public Product Product { get; set; }
+        public User User { get; set; }
+    }
+}
