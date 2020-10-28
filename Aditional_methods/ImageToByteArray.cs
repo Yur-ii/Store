3811c1672a664e061374674f45b4aca0a86bcdd8
Yur-ii <pysankoyurii@gmail.com>
2020-10-28 13:39:30 +0200

    Add default img for users

---
+++ Aditional_methods/ImageToByteArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Store.Models;
+using Store.ViewModels;
+
+namespace Store.Aditional_methods
+{
+    class DefaultImg
+    {
+        public static  byte[] UseDefaulImg()
+        {
+            string path = "wwwroot/Files/intro.jpg";
+            using (var img = new Bitmap(Image.FromFile(path)))
+            {
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    img.Save(mStream, img.RawFormat);
+                    return mStream.ToArray();
+                }
+            }
+        }
+    }
+}
